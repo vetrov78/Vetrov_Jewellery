1707e7d7faa222b5278bab915d278bea354e9b11
Andrei Vetrov <vettrov78@gmail.com>
2021-11-30 17:41:06 +0300

    JS магазина готов

--- source/js/script.js
+++ source/js/script.js
@@ -7,12 +7,10 @@
 
   // MOBILE MENU OPEN-CLOSE
   $('.page-header__menu-toggle').on('click', function () {
-    if (document.body.clientWidth < 1024) {
+    if ($(window).width() < 1024) {
       $('body').toggleClass('body-no-scroll');
       var headerHeight = $('.page-header').height() === $(window).height() ? 126 : $(window).height();
       $('.page-header').css('height', headerHeight);
-
-      // pageHeaderNavList.style.height = pageHeaderNavList.style.height === '100%' ? '' : '100%';
     }
     if ($('.page-header').hasClass('page-header--closed')) {
       $('.page-header').removeClass('page-header--closed').addClass('page-header--opened');
@@ -85,32 +83,58 @@
   });
 
   // SLIDER
-  const DESKTOP_ITEMS = 4;
-
+  // NUMBER OF ITEMS IN SHOP
+  var itemsNumber = $('.shop__product-item').length;
+  var screenItems = ( $(window).width() > 1023 ) ? 4 : 2;
+  var numberOfPages =Math.ceil(itemsNumber / screenItems);
+  // ADD FIRST PAGE ITEMS ON SCREEN
+  $('.shop__product-item').slice(0, screenItems).addClass('shop__product-item--current');
+  // ADD PAGINATION
+  if (numberOfPages > 0) {
+    if ($(window).width() > 767) {
+      $('.shop__page-number-list').append('<li class="shop__page-number-item shop__page-number-item--current" tabindex="0">1</li>');
+      for (var i = 2; i < numberOfPages + 1; i++) {
+        $('.shop__page-number-list').append(`<li class="shop__page-number-item" tabindex="0">${i}</li>`);
+      }
+    } else {
+      $('.shop__page-number-list').append(`<span class="shop__mobile-pages-pagination">1 of ${numberOfPages}</span>`);
+    }
+  }
   var currentPage = 1;
 
   var getPagNumber = function (element) {
     return Number(element.innerText);
   };
 
-  var changeCurrentPage = function (pageNumber) {
+  var changeCurrentPage = function (pageNumber, itemsOnScreen) {
     $('.shop__product-item--current').removeClass('shop__product-item--current');
-    var startItemNumber = DESKTOP_ITEMS * (pageNumber - 1);
-    var finishItemNumber = DESKTOP_ITEMS * pageNumber;
+    var startItemNumber = itemsOnScreen * (pageNumber - 1);
+    var finishItemNumber = itemsOnScreen * pageNumber;
     $('.shop__product-item').slice(startItemNumber, finishItemNumber).addClass('shop__product-item--current');
     currentPage = pageNumber;
-    $('.shop__page-number-item--current').removeClass('shop__page-number-item--current');
-    $('.shop__page-number-item').slice(pageNumber-1, pageNumber).addClass('shop__page-number-item--current');
+    if ($(window).width() > 767) {
+      $('.shop__page-number-item--current').removeClass('shop__page-number-item--current');
+      $('.shop__page-number-item').slice(pageNumber-1, pageNumber).addClass('shop__page-number-item--current');
+    } else {
+      $('.shop__page-number-list').empty();
+      $('.shop__page-number-list').append(`<span class="shop__mobile-pages-pagination">${currentPage} of ${numberOfPages}</span>`);
+    }
   }
 
-  $('.shop__arrow shop__arrow--left').on('click', function () {
-
+  $('.shop__arrow--left').on('click', function () {
+    if (currentPage > 1) {
+      changeCurrentPage(currentPage-1, screenItems);
+    }
   });
-
+  $('.shop__arrow--right').on('click', function () {
+    if (currentPage < numberOfPages) {
+      changeCurrentPage(currentPage+1, screenItems);
+    }
+  });
   $('.shop__page-number-item').on('click', function (evt) {
     var pageNumber = getPagNumber(evt.target);
     if (pageNumber !== currentPage) {
-      changeCurrentPage(pageNumber);
+      changeCurrentPage(pageNumber, screenItems);
     }
   });
 
